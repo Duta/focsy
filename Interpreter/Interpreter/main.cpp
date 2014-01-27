cd7d54f31ba9a4b88f7b4956174035ac5d4a39df
Bertie Wheen <wheen.b@gmail.com>
2014-01-27 14:45:01 +0000

    Fixed toktype2str

--- Interpreter/Interpreter/main.cpp
+++ Interpreter/Interpreter/main.cpp
@@ -10,41 +10,18 @@ void pause() {
 
 std::string toktype2str(Token *token) {
     switch(token->type) {
-    case TokenType::L_PAREN:
-        std::cout << "L_PAREN";
-        break;
-    case TokenType::R_PAREN:
-        std::cout << "R_PAREN";
-        break;
-    case TokenType::L_SQ_BRACK:
-        std::cout << "L_SQ_BRACK";
-        break;
-    case TokenType::R_SQ_BRACK:
-        std::cout << "R_SQ_BRACK";
-        break;
-    case TokenType::L_CURLY:
-        std::cout << "L_CURLY";
-        break;
-    case TokenType::R_CURLY:
-        std::cout << "R_CURLY";
-        break;
-    case TokenType::COMMA:
-        std::cout << "COMMA";
-        break;
-    case TokenType::INT_NUM:
-        std::cout << "INT_NUM";
-        break;
-    case TokenType::FLOAT_NUM:
-        std::cout << "FLOAT_NUM";
-        break;
-    case TokenType::IDENT:
-        std::cout << "IDENT";
-        break;
-    case TokenType::WHITESPACE:
-        std::cout << "WHITESPACE";
-        break;
-    default:
-        std::cout << "<unknown>";
+    case TokenType::L_PAREN:    return "L_PAREN";
+    case TokenType::R_PAREN:    return "R_PAREN";
+    case TokenType::L_SQ_BRACK: return "L_SQ_BRACK";
+    case TokenType::R_SQ_BRACK: return "R_SQ_BRACK";
+    case TokenType::L_CURLY:    return "L_CURLY";
+    case TokenType::R_CURLY:    return "R_CURLY";
+    case TokenType::COMMA:      return "COMMA";
+    case TokenType::INT_NUM:    return "INT_NUM";
+    case TokenType::FLOAT_NUM:  return "FLOAT_NUM";
+    case TokenType::IDENT:      return "IDENT";
+    case TokenType::WHITESPACE: return "WHITESPACE";
+    default: return "<unknown>";
     }
 }
 
